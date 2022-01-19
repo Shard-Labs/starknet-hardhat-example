e03efafd6465bb706319e8a51ecf880b789ac629
FabijanC <fabijan.corak@gmail.com>
2022-01-19 18:52:01 +0100

    Use active venv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,13 +6,11 @@ import "@shardlabs/starknet-hardhat-plugin";
  */
 const config: HardhatUserConfig = {
   cairo: {
-    version: "0.6.2" // alternatively choose one of the two venv options below
-
     // uses (my-venv) defined by `python -m venv path/to/my-venv`
     // venv: "path/to/my-venv"
     
     // uses the currently active Python environment (hopefully with available Starknet commands!) 
-    // venv: "active"
+    venv: "active"
   },
   networks: {
     devnet: {
